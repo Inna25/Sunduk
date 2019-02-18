35754471b0a015e685d88c839f7a196c16484961
Inna25 <47337243+Inna25@users.noreply.github.com>
2019-02-18 16:04:18 +0200

    Revert "Feature/1 1"

--- src/com/javaguru/shoppinglist/ShoppingListApplication.java
+++ src/com/javaguru/shoppinglist/ShoppingListApplication.java
@@ -1,20 +1,12 @@
 package com.javaguru.shoppinglist;
 
-import com.javaguru.shoppinglist.Validator.*;
 import java.util.ArrayList;
 import java.util.List;
 
 class ShoppingListApplication {
 
     public static void main(String[] args) {
-        Validation[] validations = new Validation[]{
-            new MinPrice(),
-            new MaxDiscount(),
-            new NameLength()
-        };
-
-        ProductValidator productValidator = new ProductValidator(validations);
-        ProductService productService = new DefaultProductService(productValidator);
+        ProductService productService = new DefaultProductService();
 
         Action exitAction = new ExitAction();
         Action createUserAction = new CreateProductAction(productService);
