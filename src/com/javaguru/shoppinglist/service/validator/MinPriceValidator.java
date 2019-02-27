c01dee3527f4c5c16472021145b08db656f6a2b7
Inna <inna2004@inbox.ru>
2019-02-27 13:11:11 +0200

    Remove check on null from validation Price & Discount Add 2 tests.

--- src/com/javaguru/shoppinglist/service/validator/MinPriceValidator.java
+++ src/com/javaguru/shoppinglist/service/validator/MinPriceValidator.java
@@ -2,21 +2,10 @@ package com.javaguru.shoppinglist.service.validator;
 
 import com.javaguru.shoppinglist.database.Product;
 
-import java.math.BigDecimal;
-
 public class MinPriceValidator implements Validation {
     @Override
     public void validate(Product newProduct) {
-        BigDecimal price = newProduct.getPrice();
-        if (assertNotNull(price.toString())) {
-            minPrice(price);
-        } else {
-            throw new FieldValidationException("The field Price can't be empty");
-        }
-    }
-
-    private void minPrice(BigDecimal price) {
-        if (price.signum() != 1) {
+        if (newProduct.getPrice().signum() != 1) {
             throw new FieldValidationException("Price must be more than 0");
         }
     }
---
+++ tests/com/javaguru/shoppinglist/service/validator/MaxDiscountValidatorTest.java
@@ -0,0 +1,37 @@
+package com.javaguru.shoppinglist.service.validator;
+
+import com.javaguru.shoppinglist.database.Product;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import java.math.BigDecimal;
+
+public class MaxDiscountValidatorTest {
+
+    @Rule
+    public final ExpectedException expectedException = ExpectedException.none();
+    private MaxDiscountValidator victim = new MaxDiscountValidator();
+    private Product input;
+
+    @Test
+    public void shouldThrowDiscountValidationException() {
+        input = product(new BigDecimal("110"));
+        expectedException.expect(FieldValidationException.class);
+        expectedException.expectMessage("Discount must be less than 100%");
+        victim.validate(input);
+    }
+
+    @Test
+    public void shouldValidateSuccess() {
+        input = product(new BigDecimal("5"));
+        victim.validate(input);
+    }
+
+    private Product product(BigDecimal price) {
+        Product product = new Product();
+        product.setPrice(price);
+        return product;
+    }
+
+}
